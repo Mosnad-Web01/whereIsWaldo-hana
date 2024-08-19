8831a31b7576d49a3b0b858ac86eb7a55f940903
Muhanned Noman <Muhanned.Noman@gmail.com>
2024-08-19 11:35:15 +0300

    feat: create main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+const example = [
+  ["A","A","A"],
+  ["A","A","A"],
+  ["B","A","A"]
+];
+
+
+const whereIsWaldo = (matrix) => {
+  
+}
+
+console.log(whereIsWaldo(example))
+// should output [3,1]
+
